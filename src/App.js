dac4f976004b9ec7daa1e144cbb281750741f280
Shravan Chaudhari <shravanchaudhari99@gmail.com>
2021-06-14 11:59:01 +0530

    country cards

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { useState, useEffect } from "react";
 import Card from "./components/Card";
 import Loading from "./components/Loading";
 import "./App.css";
+import styled from "styled-components";
 
 function App() {
   const [error, setError] = useState(null);
@@ -37,21 +38,58 @@ function App() {
   } else {
     return (
       <div className="App">
-        <div className="countries">
+        <Countries>
           {items.map((item) => (
             <Card
-              key={item.callingCodes}
+              key={item.numericCode}
               img={item.flag}
+              callingCode={item.callingCodes[0]}
+              language={item.languages[0].name}
               name={item.name}
+              currencies={item.currencies[0].name}
               population={item.population}
               region={item.region}
               capital={item.capital}
             />
           ))}
-        </div>
+        </Countries>
       </div>
     );
   }
 }
 
 export default App;
+const Countries = styled.div`
+  @media (min-width: 1600px) {
+    display: grid;
+    grid-template-columns: 400px 400px 400px 400px;
+    justify-content: center;
+  }
+  @media (min-width: 1200px) and (max-width: 1600px) {
+    display: grid;
+    grid-template-columns: 400px 400px 400px;
+    justify-content: center;
+  }
+
+  @media (max-width: 1200px) {
+    display: grid;
+    grid-template-columns: 400px 400px;
+    justify-content: center;
+  }
+
+  @media (max-width: 800px) {
+    display: grid;
+    grid-template-columns: 300px 300px;
+    justify-content: center;
+  }
+  @media (max-width: 500px) and (min-width: 400px) {
+    display: grid;
+    grid-template-columns: 200px 200px;
+    justify-content: center;
+  }
+  @media (max-width: 400px) {
+    display: grid;
+    grid-template-columns: 200px;
+    justify-content: center;
+  }
+`;
--- src/components/Card.js
+++ src/components/Card.js
@@ -1,14 +1,26 @@
 import React from "react";
 import styled from "styled-components";
 
-function Card({ img, name, population, region, capital }) {
+function Card({
+  img,
+  name,
+  callingCode,
+  population,
+  region,
+  capital,
+  currencies,
+  language,
+}) {
   return (
     <Container>
       <Flag src={img} />
       <Title>
-        <h1>{name}</h1>
+        <h3>{name}</h3>
       </Title>
       <Info>
+        <p>Calling Code: {callingCode}</p>
+        <p>Language Spoken: {language}</p>
+        <p>Currency: {currencies}</p>
         <p>Population: {population}</p>
         <p>Capital: {capital}</p>
         <p>Region: {region}</p>
@@ -18,7 +30,45 @@ function Card({ img, name, population, region, capital }) {
 }
 
 export default Card;
-const Container = styled.div``;
+const Container = styled.div`
+  @media (min-width: 800px) {
+    display: flex;
+    flex-direction: column;
+    padding: 20px;
+    margin: 10px;
+    max-width: 300px;
+    background-color: #cfcfcf;
+    border-radius: 10px;
+    box-shadow: rgba(100, 100, 111, 0.2) 0px 7px 29px 0px;
+    :hover {
+      cursor: pointer;
+      transition: transform 0.2s ease-in-out;
+      transform: scale(1.02) !important;
+    }
+  }
+  @media (max-width: 800px) {
+    display: flex;
+    flex-direction: column;
+    padding: 20px;
+    margin: 10px;
+    max-width: 250px;
+    background-color: cfcfcf;
+    border-radius: 10px;
+    box-shadow: rgba(100, 100, 111, 0.2) 0px 7px 29px 0px;
+  }
+  @media (max-width: 400px) {
+    display: flex;
+    flex-direction: column;
+    padding: 20px;
+    margin: 10px;
+    max-width: 150px;
+    background-color: cfcfcf;
+    border-radius: 10px;
+    box-shadow: rgba(100, 100, 111, 0.2) 0px 7px 29px 0px;
+  }
+`;
 const Flag = styled.img``;
-const Title = styled.div``;
+const Title = styled.div`
+  margin: 15px 15px 15px 15px;
+`;
 const Info = styled.div``;
